b81f544f1825510ae55b8020a4f8a3ae9c7381c8
Valdivia, Jaime H <jaime.valdivia@astrazeneca.com>
2018-08-20 09:04:56 -0500

    Finished product

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let questionBank = QuestionBank()
+    var questionNumber: Int = 0
+    var score: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,33 +22,57 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setQuestionLabel()
+        updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        checkAnswer(pickedAnswer: sender.tag == 1)
+        questionNumber += 1
+        nextQuestion()
     }
     
     
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
 
     func nextQuestion() {
-        
+        if (questionNumber <= questionBank.list.count - 1) {
+            setQuestionLabel()
+            updateUI()
+        } else {
+            let alert = UIAlertController(title: "Completed", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in self.startOver() })
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
     }
     
-    
-    func checkAnswer() {
-        
+    func checkAnswer(pickedAnswer: Bool) {
+        if questionBank.list[questionNumber].answer == pickedAnswer {
+            score += 1
+            ProgressHUD.showSuccess("Correct")
+        } else {
+            ProgressHUD.showError("Wrong")
+        }
     }
-    
     
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        nextQuestion()
+        updateUI()
     }
     
-
+    func setQuestionLabel() {
+        questionLabel.text = questionBank.list[questionNumber].questionText
+    }
     
 }
